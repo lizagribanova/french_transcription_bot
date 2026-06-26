92791da5ee75335aefb99a52b15c16f03bf4c6f7
lizagribanova <elzttv@gmail.com>
2026-06-26 16:40:12 +0300

    Создана структура проекта

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print('Это тестовый коммит')
+import requests
+import time
+import os
+from environs import Env
+
+env = Env()  
+env.read_env() 
+                          
+bot_token = env('BOT_TOKEN')
+API_URL = 'https://api.telegram.org/bot'
+TEXT = 'Тестирую ботика'
+MAX_COUNTER = 100
+
+offset = -2
+counter = 0
+chat_id: int
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{bot_token}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{bot_token}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
